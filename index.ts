77185052a57c6df2033152ccbd2ded164bdc1c7a
Decagon <deca@macs-MacBook-Air.local>
2023-03-17 17:54:19 +0100

    Project first-commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+const reviewTotalDisplay = document.querySelector('#reviews')
+
+const reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+];
+
+//write a function that shows the number of reviews in the website and it takes a number
